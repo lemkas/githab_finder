ece07784a5dc44f4554668d9e1b8f80e3e061e23
lemkas <lemov1980@yandex.ru>
2021-09-18 13:37:52 +0300

    make cards for users

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+const Navbar = ({ title }) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>{title}</h1>
+    </nav>
+  );
+};
+
+Navbar.defaultProps = {
+  title: "Github finder",
+};
+
+export default Navbar;
